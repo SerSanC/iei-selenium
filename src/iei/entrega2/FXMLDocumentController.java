053ea4634fadbd8c24828fe86c241b666ba52150
Cristian Zazo <cristian.zazo@gmail.com>
2018-11-21 11:01:35 +0100

    Mazo de movidas y fixes de ECI

--- src/iei/entrega2/FXMLDocumentController.java
+++ src/iei/entrega2/FXMLDocumentController.java
@@ -7,13 +7,17 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -63,20 +67,39 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         grid_identificador = new GridPane();
         InicializarGridPane();
+        checkAmazon.setDisable(true);
+        
+        checkFnac.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                checkFnac.setSelected(newValue);
+                checkElCorteIngles.setSelected(!newValue);
+            }
+        });
+
+        checkElCorteIngles.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                checkElCorteIngles.setSelected(newValue);
+                checkFnac.setSelected(!newValue);
+            }
+        });
     }
 
     @FXML
     private void Buscar(ActionEvent event) {
-        if (checkFnac.isSelected()) {
+        LimpiarGridPane();
+        InicializarGridPane();
+        i = 1;
+        
+        if (checkFnac.isSelected() && !checkElCorteIngles.isSelected()) {
             Fnac(t_libro.getText(), autor.getText());
         }
-        if (checkAmazon.isSelected()) {
-            Amazon(t_libro.getText(), autor.getText());
+        if (checkElCorteIngles.isSelected() && !checkFnac.isSelected()) {
+            ECI(t_libro.getText(), autor.getText());
         }
-        if (checkAmazon.isSelected() && checkFnac.isSelected()) {
-            Amazon(t_libro.getText(), autor.getText());
-            Fnac(t_libro.getText(), autor.getText());
-        }
+        
+        grid_identificador.setVisible(true);
     }
 
     public void Fnac(String titulo, String autor) {
@@ -113,12 +136,6 @@ public class FXMLDocumentController implements Initializable {
     }
 
     private void Fnac_Titulo(String titulo) {
-        LimpiarGridPane();
-        InicializarGridPane();
-        if (contador_busqueda > 0) {
-            i = 1;
-        }
-
         tamaño = driver.findElements(By.className("Article-item")).size();
 
         do {
@@ -173,10 +190,6 @@ public class FXMLDocumentController implements Initializable {
     }
 
     private void Fnac_Autor(String autor) {
-        LimpiarGridPane();
-        InicializarGridPane();
-        i = 1;
-
         WebDriverWait waiting = new WebDriverWait(driver, 10);
         waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("Action-btn")));
         WebElement fichaBtn = driver.findElement(By.className("Action-btn"));
@@ -243,7 +256,7 @@ public class FXMLDocumentController implements Initializable {
         }
     }
 
-    private void Amazon(String libro, String autor) {
+    private void ECI(String titulo, String autor) {
         String exePath = "lib/chromedriver";
         System.setProperty("webdriver.chrome.driver", exePath);
         ChromeOptions options = new ChromeOptions();
@@ -251,81 +264,10 @@ public class FXMLDocumentController implements Initializable {
         driver = new ChromeDriver(options);
         driver.get("http://www.elcorteingles.es");
 
-        if (!libro.isEmpty()) {
-            WebElement element = driver.findElement(By.id("search-box"));
-            element.sendKeys(libro);
-            element.submit();
-
-            WebDriverWait waiting = new WebDriverWait(driver, 10);
-
-            driver.findElement(By.id("cookies-agree")).click();
-
-            waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("facet-popup")));
-            driver.findElements(By.className("facet-popup")).get(1).click();
-
-            try {
-                waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("js-product-click")));
-            } catch (Exception e) {
-                LimpiarGridPane();
-                label = new Text("Libro no encontrado");
-                vbox_identificador.getChildren().add(label);
-                //     break;
-            }
-            int cont = 0;
-
-            int j = 0;
-            for (WebElement el : driver.findElements(By.className("product-preview"))) {
-
-                String titles = driver.findElements(By.className("js-product-click")).get(j).getText();
-                System.out.println("Titulos2" + titles);
-                String precioAct = el.findElement(By.className("current")).getText();
-                try {
-                    precioAnt = el.findElement(By.className("former")).getText();
-                } catch (Exception e) {
-                    precioAnt = " ";
-                }
-                grid_identificador.addColumn(0, new Label(" El Corte Ingles "));
-
-                if (titles.length() >= 80) {
-                    titles = titles.substring(0, 80);
-                }
-                grid_identificador.add(new Label(" " + titles + " "), 1, i);
-                grid_identificador.addColumn(2, new Label(precioAnt));
-                grid_identificador.addColumn(3, new Label(precioAct));
-                j++;
-                i++;
-
-            }
-        }
-        if (!autor.isEmpty()) {
-            WebElement element = driver.findElement(By.id("search-box"));
-            element.sendKeys(autor);
-            element.submit();
-
-            WebDriverWait waiting = new WebDriverWait(driver, 10);
-
-            driver.findElement(By.id("cookies-agree")).click();
-
-            waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("facet-popup")));
-            driver.findElements(By.className("facet-popup")).get(0).click();
-
-            try {
-                waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("js-product-click")));
-            } catch (Exception e) {
-                LimpiarGridPane();
-                label = new Text("Libro no encontrado");
-                vbox_identificador.getChildren().add(label);
-                //     break;
-            }
-            int cont = 0;
-            //  do {
-            for (WebElement el : driver.findElements(By.className("js-product-click"))) {
-                el.click();
-                System.out.println("Llegamos al break");
-
-                break;
-            }
-
+        if (!titulo.isEmpty()) {
+            ECI_Titulo(titulo);
+        } else if (!autor.isEmpty()) {
+            ECI_Autor(autor);
         }
 
         grid_identificador.setAlignment(Pos.CENTER);
@@ -334,6 +276,9 @@ public class FXMLDocumentController implements Initializable {
         vbox_identificador.setAlignment(Pos.CENTER);
         grid_identificador.setGridLinesVisible(true);
         grid_identificador.setVisible(true);
+        
+        driver.close();
+        driver.quit();
     }
 
     private void InicializarGridPane() {
@@ -350,14 +295,90 @@ public class FXMLDocumentController implements Initializable {
         grid_identificador = new GridPane();
     }
 
-    private void ECI_Titulo(String libro) {
+    private void ECI_Titulo(String titulo) {        
+        WebElement element = driver.findElement(By.id("search-box"));
+        element.sendKeys(titulo);
+        element.submit();
+
         WebDriverWait waiting = new WebDriverWait(driver, 10);
 
+        driver.findElement(By.id("cookies-agree")).click();
+
+        waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("facet-popup")));
+        driver.findElements(By.className("facet-popup")).get(1).click();
+
+        try {
+            waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("js-product-click")));
+        } catch (Exception e) {
+            LimpiarGridPane();
+            label = new Text("Libro no encontrado");
+            vbox_identificador.getChildren().add(label);
+            //     break;
+        }
+        boolean last_page = false;
+        do {
+            waiting.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("product-preview")));
+            for (WebElement el : driver.findElements(By.className("product-preview"))) {
+                String titles = el.findElement(By.className("js-product-click")).getText();
+                System.out.println(titles);
+                String precioAct = el.findElement(By.className("current")).getText();
+                try {
+                    precioAnt = el.findElement(By.className("former")).getText();
+                } catch (Exception e) {
+                    precioAnt = "";
+                }
+                if(precioAnt.equals("")) {
+                    precioAnt = precioAct;
+                    precioAct = "";
+                }
+                grid_identificador.addColumn(0, new Label(" El Corte Ingles "));
+
+                if (titles.length() >= 80) {
+                    titles = titles.substring(0, 80);
+                }
+                grid_identificador.add(new Label(" " + titles + " "), 1, i);
+                grid_identificador.addColumn(2, new Label(precioAnt));
+                grid_identificador.addColumn(3, new Label(precioAct));
+                i++;
+            }
+            try {
+                driver.findElements(By.cssSelector("div.pagination>ul li:last-child>a")).get(0).click();
+                System.out.println("Next");
+            } catch (Exception e) {
+                last_page = true;
+                System.out.println("Última página");
+            }
+        } while (!last_page);
     }
 
     private void ECI_Autor(String autor) {
+        WebElement element = driver.findElement(By.id("search-box"));
+        element.sendKeys(autor);
+        element.submit();
+
         WebDriverWait waiting = new WebDriverWait(driver, 10);
 
+        driver.findElement(By.id("cookies-agree")).click();
+
+        waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("facet-popup")));
+        driver.findElements(By.className("facet-popup")).get(0).click();
+
+        try {
+            waiting.until(ExpectedConditions.presenceOfElementLocated(By.className("js-product-click")));
+        } catch (Exception e) {
+            LimpiarGridPane();
+            label = new Text("Libro no encontrado");
+            vbox_identificador.getChildren().add(label);
+            //     break;
+        }
+        int cont = 0;
+        //  do {
+        for (WebElement el : driver.findElements(By.className("js-product-click"))) {
+            el.click();
+            System.out.println("Llegamos al break");
+
+            break;
+        }
     }
 
 }
